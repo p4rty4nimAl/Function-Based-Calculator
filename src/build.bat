7bd606269b0ad28814023f9799e38a65362518fe
p4rty4nimAl <69524746+p4rty4nimAl@users.noreply.github.com>
2022-10-01 12:42:58 +0100

    use as a ref: but you have to make your own now :D

--- src/build.bat
+++
@@ -1,6 +0,0 @@
-javac -d out/ -cp . tk/p4rty/Main.java
-cd out
-"C:\Program Files\Eclipse Adoptium\jdk-17.0.1.12-hotspot\bin\jar.exe" cmf fbc.mf FBC.jar tk/*
-move FBC.jar ../FBC.jar
-cd ../
-cmd java -jar FBC.jar
